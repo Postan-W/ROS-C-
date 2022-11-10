4a258e2c1677da4a8652facdb546cf801de6bd69
wmz <18363099526@163.com>
2022-11-10 22:58:16 +0800

    first_package

---
+++ space1/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/wmz/桌面/ros_workspace/space1/src
+# Build directory: /home/wmz/桌面/ros_workspace/space1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("hello_ros")
---
+++ space1/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/wmz/桌面/ros_workspace/space1/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/wmz/桌面/ros_workspace/space1/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/wmz/桌面/ros_workspace/space1/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/wmz/桌面/ros_workspace/space1/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/wmz/桌面/ros_workspace/space1/src:$ROS_PACKAGE_PATH"
