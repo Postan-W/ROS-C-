3ea64100a09ec21c4e32a7e978b3a7e0676808eb
wmz <18363099526@163.com>
2022-11-13 23:32:54 +0800

    first message

--- space1/src/hello_topic/scripts/publisher_node.py
+++ space1/src/hello_topic/scripts/publisher_node.py
@@ -1,3 +1,19 @@
 #!/usr/env/bin python
 # coding: utf-8
 import rospy
+from std_msgs.msg import String
+
+if __name__ == '__main__':
+    rospy.init_node('publisher_node')
+
+    topic_name = 'hello'
+    publisher = rospy.Publisher(topic_name,String,queue_size=100)#topic名称、数据类型、消息大小
+
+    count = 0
+    rate = rospy.Rate(4)#一秒4次。下面调用sleep的时候，相当于让程序sleep了1/4秒的时间才继续执行
+    while not rospy.is_shutdown():
+        msg = String()
+        msg.data = 'hello topic {}'.format(count)
+        publisher.publish(msg)
+        rate.sleep()
+        count += 1
